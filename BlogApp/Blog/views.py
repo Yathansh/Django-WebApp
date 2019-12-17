82158e63d6358b01d5f6299418549c5b348e61bb
Yathansh <47215512+Yathansh@users.noreply.github.com>
2019-12-17 23:07:08 +0530

    Templates added

--- BlogApp/Blog/views.py
+++ BlogApp/Blog/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts=[
+	{
+		'author': 'User1',
+		'title': 'Blog post 1',
+		'content': 'User1 content',
+		'date_post': '17  December,2019',
+	},
+	{
+		'author': 'User2',
+		'title': 'Blog post 2',
+		'content': 'User2 content',
+		'date_post': '17  December,2019',
+	}
+]
 def home(request):
-	return HttpResponse('<h1>BLOG HOMEPAGE</h1>')
+	context = {
+		'posts':posts
+	}
+	return render(request, 'Blog/homepage.html', context)
 
 def about(request):
-	return HttpResponse('<h1>ABOUT BLOG</h1>')
+	return render(request, 'Blog/aboutpage.html',{'title': 'About'})
